3164dcbdeb24464f3c49d443e2a8bd2f0ea4de58
Luis <fasterplop@gmail.com>
2026-01-23 12:17:14 -0400

    cloudflare added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,23 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://globocareremodeling.com',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
